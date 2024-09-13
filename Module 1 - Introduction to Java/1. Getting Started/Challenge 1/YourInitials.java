83ded8fd8ac609f2487360219d7d2ce54bd04849
weeranieb <weerachai.panthueng@gmail.com>
2024-09-13 19:10:40 +0700

    finish challenge

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("W   W   PPPP ");
+         System.out.println("W   W   P   P");
+         System.out.println("W   W   P   P");
+         System.out.println("W W W   PPPP ");
+         System.out.println("WW WW   P    ");
+         System.out.println("WW WW   P    ");
+         System.out.println("W   W   P    ");
     }
 }
