9dd9a10415e2c2e95dcda688639de3e051e3e8a3
weeranieb <weerachai.panthueng@gmail.com>
2024-09-20 16:45:20 +0700

    do quiz

--- Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -1,4 +1,4 @@
-//import Scanner
+import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
@@ -7,34 +7,33 @@ public class Chatbot {
 
         
         //set up scanner. 
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Hello. What is your name?");
-        //Pick up user's name and store it. 
+        String name = scan.nextLine();
 
                            
         //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
-        //Pick up user's home and store it. 
-                        
+        System.out.printf("Hi %s! I'm Javabot. Where are you from?\n", name);
+        String home = scan.nextLine();
 
         //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
+        System.out.printf("I hear it's beautiful at %s! I'm from a place called Oracle\n", home);
         System.out.println("How old are you?");
-        //Pick up age and store it.
-
+        int age = scan.nextInt();
+        scan.nextLine();
 
         //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.printf("So you're %d, cool! I'm 400 years old.\n", age);
+        System.out.printf("This means I'm %f times older than you.\n", 400/(double)age);
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
-        //Pick up language and store it. 
-
+        String lang = scan.nextLine();
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.printf("%s, that's great! Nice chatting with you %s. I have to log off now. See ya!\n", lang, name);
         
         //close scanner. 
-
+        scan.close();
         
     }
 }
--- Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
@@ -8,10 +8,10 @@ public class Meeting {
         double refunds = 688.78; 
         double shipping = 1233.57; 
 
-        System.out.println("This month, we made $<sales> in sales");
-        System.out.println("Factoring in costs, we made $<profit> in profit");
-        System.out.println("The refunds are at a low $<refunds>. This is a good sign!");
-        System.out.println("Shipping costs were high. We paid $<shipping> in shipping");
+        System.out.printf("This month, we made $%d in sales\n", (int)sales);
+        System.out.printf("Factoring in costs, we made $%d in profit\n", (int)profit);
+        System.out.printf("The refunds are at a low $%d. This is a good sign!\n", (int)refunds);
+        System.out.printf("Shipping costs were high. We paid $%d in shipping\n", (int)shipping);
 
 
     }
